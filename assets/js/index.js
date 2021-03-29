83b9e39bbee381116b1fb27a3a0154e62dac3db1
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-03-29 20:39:57 +0000

    Added basic levels, link to the how to play page, footer, and basic modal popup when index.html loads.

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#exampleModalCenter').modal('show');
+    $('.close-modal').click(function (event) {
+        $('#exampleModalCenter').modal('hide');
+    })
+})
