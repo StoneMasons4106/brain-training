2e0bbf8d8a6c1fe0fd60f6ed608c827e7eaefd66
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-03-31 01:57:48 +0000

    Added darkmode js, reformatted the level layout on the home page to emulate a stoplight. Made the header items buttons.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
-    $('#exampleModalCenter').modal('show');
-    $('.close-modal').click(function (event) {
+    let count;
+    count = 0;
+
+    $('#how-to-play').click(function () {
+        $('#exampleModalCenter').modal('show');
+    })
+    
+    $('.close-modal').click(function () {
         $('#exampleModalCenter').modal('hide');
+    })
+
+    $('#darkmode').click(function () {
+        count = count + 1;
+        if (count % 2 === 0) {
+            $('#darkmode-link').remove();
+        } else {
+            $(document.head).append('<link id="darkmode-link" href="assets/css/darkmode.css" rel="stylesheet" type="text/css">');
+        }
     })
 })
