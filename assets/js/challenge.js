2634bd1f464bcbee23fbda53f55fa1efeb93cded
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-04-01 19:22:34 +0000

    Gave the challenge page the same page as the header, added a home button to the challenge page. Made a challenge.js that loads darkmode preference from local storage.

---
+++ assets/js/challenge.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    var count = localStorage.getItem('count') || 0;
+    
+    if (count % 2 === 0) {
+        $('#darkmode-link').remove();
+    } else {
+        $(document.head).append('<link id="darkmode-link" href="assets/css/darkmode.css" rel="stylesheet" type="text/css">');
+    }
+
+    $('#darkmode-button, #darkmode-dropdown').click(function () {
+        count = parseInt(count) + 1;
+        if (count % 2 === 0) {
+            $('#darkmode-link').remove();
+            localStorage.setItem('count', count);
+        } else {
+            $(document.head).append('<link id="darkmode-link" href="assets/css/darkmode.css" rel="stylesheet" type="text/css">');
+            localStorage.setItem('count', count);
+        }
+    })
+})
