0bfbd479de83db8246d3a22a19968f1c3a377fa0
StoneMasons4106 <jake.d.rubach@gmail.com>
2021-04-02 20:16:10 +0000

    Gave the challenge page the background image of a chess board, gave it a footer, and wrote the js logic for the site to know what level was selected via js and not html.

--- assets/js/challenge.js
+++ assets/js/challenge.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var count = localStorage.getItem('count') || 0;
-    
+    var level = localStorage.getItem('level');
+
     if (count % 2 === 0) {
         $('#darkmode-link').remove();
     } else {
@@ -17,4 +18,12 @@ $(document).ready(function () {
             localStorage.setItem('count', count);
         }
     })
+
+    if (level == 3) {
+        $('#training-image').after('<p>Level 3</p>')
+    } else if (level == 2) {
+        $('#training-image').after('<p>Level 2</p>')
+    } else if (level == 1) {
+        $('#training-image').after('<p>Level 1</p>')
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var count = localStorage.getItem('count') || 0;
+    var level = 0;
     
     if (count % 2 === 0) {
         $('#darkmode-link').remove();
@@ -24,5 +25,20 @@ $(document).ready(function () {
             $(document.head).append('<link id="darkmode-link" href="assets/css/darkmode.css" rel="stylesheet" type="text/css">');
             localStorage.setItem('count', count);
         }
+    })
+
+    $('#level-1-button').click(function () {
+        level = 1;
+        localStorage.setItem('level', level);
+    })
+
+    $('#level-2-button').click(function () {
+        level = 2;
+        localStorage.setItem('level', level);
+    })
+
+    $('#level-3-button').click(function () {
+        level = 3;
+        localStorage.setItem('level', level);
     })
 })
